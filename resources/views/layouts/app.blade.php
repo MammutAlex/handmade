0dcf24f4dc17c4cdf3622ec7ea4324c0316dbfd5
mammut <mammutcris@gmail.com>
2016-09-13 18:00:19 +0300

    Add doctype

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,9 @@
-<html>
+<!DOCTYPE html>
 <head>
     <meta http-equiv="Content-Type"
           content="text/html; charset=UTF-8">
-
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1">
     <!-- Page Title -->
-    <title>Dream-made  - Тетяна Ковальчук</title>
+    <title>Dream-made - Тетяна Ковальчук</title>
     <!-- Fonts -->
     <link href="/css/css"
           rel="stylesheet"
@@ -91,10 +88,17 @@
 <script src="/js/plugin.js"></script>
 <script src="/js/main.js"></script>
 <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-83914054-1', 'auto');
     ga('send', 'pageview');
