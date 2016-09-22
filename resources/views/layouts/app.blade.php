0aea23b978d9f595c1c5732934d17c760e9e45a8
mammut <mammutcris@gmail.com>
2016-09-22 13:46:11 +0300

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <head>
     <meta http-equiv="Content-Type"
           content="text/html; charset=UTF-8">
+    <link rel="shortcut icon" href="/favicon.gif" type="image/gif">
     <!-- Page Title -->
     <title>Dream-made - Тетяна Ковальчук</title>
     <!-- Fonts -->
