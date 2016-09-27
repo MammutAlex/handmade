ea93479072d6744063825e70d91b7842c1d0382b
mammut <mammutcris@gmail.com>
2016-09-27 14:39:54 +0300

    Created portfolio page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
-Route::get('/', 'WelcomeController@index');
-Route::get('/portfolio/{portfolio}', 'WelcomeController@portfolio');
-Route::get('/blog', 'WelcomeController@blog');
-Route::get('/blog/{blog}', 'WelcomeController@blogSingle');
-Route::get('/about', 'WelcomeController@about');
-Route::get('/contact', 'WelcomeController@contact');
-Route::post('/mail-contact', 'WelcomeController@contactPost');
-Auth::routes();
+    Route::get('/', 'WelcomeController@index');
+    Route::get('/portfolio', 'WelcomeController@index');
+    Route::get('/portfolio/{portfolio}', 'WelcomeController@portfolio');
+    Route::get('/blog', 'WelcomeController@blog');
+    Route::get('/blog/{blog}', 'WelcomeController@blogSingle');
+    Route::get('/about', 'WelcomeController@about');
+    Route::get('/contact', 'WelcomeController@contact');
+    Route::post('/mail-contact', 'WelcomeController@contactPost');
+    Auth::routes();
