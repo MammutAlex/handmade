1296071cd2f50a9e92df8b6600ba3a9c084c9c71
mammut <mammutcris@gmail.com>
2016-09-30 16:25:09 +0300

    Created 404 pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', '404')
+@section('content')
+    <div class="home-photography">
+        <div class="head-photo"
+             style="background-image: url(/img/main.jpg);"></div>
+        <div class="title-holder">
+            <div class="page-title machinery-text">
+                <h2>404</h2>
+                <span>Вибачте цієї сторінки не існує</span>
+            </div>
+        </div>
+    </div>
+@endsection
